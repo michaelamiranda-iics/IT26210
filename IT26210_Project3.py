301d5ca36499bed4afabe2abb12c363d7789702c
michaelamiranda-iics <michaelaantonette.miranda.iics@ust.edu.ph>
2021-10-15 19:44:02 +0800

    Update IT26210_Project3.py

--- IT26210_Project3.py
+++ IT26210_Project3.py
@@ -1,11 +1,13 @@
 import re
 from requests import get
 
+#To check the format of the IP address both IPv4 & IPv6
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 regex2 = "^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
 
 loopVariable = True
 
+#loop for the loopVariable
 while loopVariable:
     ip_address = input("Enter IP Address: ")
     def check(Ip):
@@ -29,10 +31,10 @@ while loopVariable:
             print('ORG: ' + get('https://ipapi.co/'+ip_address+'/org/').text)
         else:
             print("Invalid IP address")
-
+    #for checking the IP address
     if __name__ == '__main__' :
         check(ip_address)
-
+    #loop for inputing another IP address
         answer = True
         while answer:
             print(" ")
